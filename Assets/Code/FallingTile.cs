bd9996f8a94bebcc4f65e6afbd4a2f84e59994cb
Kevin Ye <yekevin1995@hotmail.com>
2020-01-11 17:01:06 -0500

    WIP Menu

---
+++ Assets/Code/FallingTile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTile : MonoBehaviour
+{
+    Transform[] childs;
+    GameObject randomObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        childs = getFirstChildren(gameObject.transform);
+        InvokeRepeating("TileFalls", 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void TileFalls()
+    {
+        randomObject = (childs[Random.Range(0, childs.Length)]).gameObject;
+        randomObject.GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    private Transform[] getFirstChildren(Transform parent)
+    {
+        Transform[] children = parent.GetComponentsInChildren<Transform>();
+        Transform[] firstChildren = new Transform[parent.childCount];
+        int index = 0;
+        foreach (Transform child in children)
+        {
+            if (child.parent == parent)
+            {
+                firstChildren[index] = child;
+                index++;
+            }
+        }
+        return firstChildren;
+    }
+}
