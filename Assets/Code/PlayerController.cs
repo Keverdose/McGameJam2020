78680fd8586e3be53aab14fb2031cfbdb7cae508
Johnny Mak <johnny_mak@outlook.com>
2020-01-11 16:24:27 -0500

    Adding raycast system for the player movement

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -20,23 +20,34 @@ public class PlayerController : MonoBehaviour
     {
         if(transform.position.y >= initialY)
         {
+            Vector3 movementDirection = new Vector3(0, 0, 0);
+
             //TODO: All the inputs should be a tile size worth of movement
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-                transform.Translate(0, 0, 1);
+                movementDirection.z = 1;
+                // transform.Translate(0, 0, 1);
             }
             else if (Input.GetKeyDown(KeyCode.DownArrow))
             {
-                transform.Translate(0, 0, -1);
+                movementDirection.z = -1;
+                // transform.Translate(0, 0, -1);
             }
             else if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                transform.Translate(-1, 0, 0);
+                movementDirection.x = -1;
+                //transform.Translate(-1, 0, 0);
             }
             else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                transform.Translate(1, 0, 0);
+                movementDirection.x = 1;
+                //transform.Translate(1, 0, 0);
             }
+
+            // Check if there are any obstacle 
+            Move(movementDirection);
+            
+
         }
         else
         {
@@ -47,5 +58,21 @@ public class PlayerController : MonoBehaviour
     void Respawn()
     {
         SceneManager.LoadScene("SC_Level1");
+    }
+
+
+    // Given a direction,
+    bool Move(Vector3 moveDirection)     
+    {
+        Vector3 currentPosition = new Vector3(transform.position.x, transform.position.y / 2, transform.position.z);
+
+        // No Collision, proceed with the movement
+        if (!Physics.Raycast(currentPosition, moveDirection, 1)) 
+        {
+            transform.Translate(moveDirection);
+            return true;
+        }
+
+        return false;
     }
 }
