3eca7f531cb066aa3a012b44650d77dd71ad72fc
Johnny Mak <johnny_mak@outlook.com>
2020-01-12 13:10:39 -0500

    added timer to game manager tick

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -7,31 +7,39 @@ public class GameManager : MonoBehaviour
     public PlayerController player;
     public TempleGrid grid;
 
+    public float timeCount;
+    public float tickTime = 0.25f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeCount = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //TODO: All the inputs should be a tile size worth of movement
-        if (Input.GetKeyDown(KeyCode.UpArrow) && player.Move(new Vector3(0,0,1)))
-        {
-            grid.TickGrid();
+        if (timeCount >= tickTime) {
+
+            //TODO: All the inputs should be a tile size worth of movement
+            if (Input.GetKeyDown(KeyCode.UpArrow) && player.Move(new Vector3(0, 0, 1))) {
+                grid.TickGrid();
+                timeCount = 0.0f;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && player.Move(new Vector3(0, 0, -1))) {
+                grid.TickGrid();
+                timeCount = 0.0f;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && player.Move(new Vector3(-1, 0, 0))) {
+                grid.TickGrid();
+                timeCount = 0.0f;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && player.Move(new Vector3(1, 0, 0))) {
+                grid.TickGrid();
+                timeCount = 0.0f;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && player.Move(new Vector3(0, 0, -1)))
-        {
-            grid.TickGrid();
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) && player.Move(new Vector3(-1, 0, 0)))
-        {
-            grid.TickGrid();
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && player.Move(new Vector3(1, 0, 0)))
-        {
-            grid.TickGrid();
-        }
+
+        timeCount += Time.deltaTime;
     }
 }
