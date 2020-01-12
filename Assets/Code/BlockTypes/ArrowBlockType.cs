1c0455fd352e9d056a302e1d1e7078ff22c27e55
Kevin Ye <yekevin1995@hotmail.com>
2020-01-12 10:33:50 -0500

    WIP arrow block

--- Assets/Code/BlockTypes/ArrowBlockType.cs
+++ Assets/Code/BlockTypes/ArrowBlockType.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ArrowBlockType : TempleBlock
 {
+    public GameObject ArrowPrefab;
+    public List<GameObject> ArrowsInstantiated;
+
+    public int tickCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,14 +16,30 @@ public class ArrowBlockType : TempleBlock
         print("arrow block start");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public override void TickObject()
     {
-        print("Tick arrow");
+        if(tickCount % 4 == 1)
+        {
+            InstantiateArrow();
+        }
+        foreach(GameObject arrow in ArrowsInstantiated)
+        {
+            print(this.transform.forward);
+            arrow.transform.Translate(this.transform.forward);
+            if(arrow.transform.position.x > 7.0f || arrow.transform.position.z > 7.0f ||
+                arrow.transform.position.x < -7.0f || arrow.transform.position.z < -7.0f)
+            {
+                ArrowsInstantiated.Remove(arrow);
+                Destroy(arrow);
+            }
+        }
+        tickCount++;
+    }
+
+    public void InstantiateArrow()
+    {
+        Vector3 startingPos = this.transform.position +  new Vector3(0,1,0);
+        GameObject arrow = Instantiate(ArrowPrefab, startingPos, Quaternion.identity);
+        ArrowsInstantiated.Add(arrow);
     }
 }
