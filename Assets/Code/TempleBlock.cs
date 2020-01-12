cda68f6c27539c48509acdfa1ea7061d940f5247
Johnny Mak <johnny_mak@outlook.com>
2020-01-11 19:08:42 -0500

    Added timer to the tile player is standing on

--- Assets/Code/TempleBlock.cs
+++ Assets/Code/TempleBlock.cs
@@ -6,25 +6,51 @@ public class TempleBlock : MonoBehaviour
 {
     Rigidbody rb;
 
+    float playerOnTileTimerLimit = 4.0f;
+    float playerOnTileTimer = 0.0f;
+    bool isPlayerOnTile; 
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+        isPlayerOnTile = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isPlayerOnTile) 
+        {
+            playerOnTileTimer += Time.deltaTime;
+
+            if(playerOnTileTimer > playerOnTileTimerLimit) 
+            {
+                DestroyTile();
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isPlayerOnTile = true;
+        }
     }
 
     private void OnCollisionExit(Collision other)
     {
         if(other.gameObject.tag == "Player")
         {
-            rb.isKinematic = false;
-            Destroy(gameObject,1f);
+            isPlayerOnTile = false;
+            DestroyTile();
         }
+    }
+
+    private void DestroyTile() 
+    {
+        rb.isKinematic = false;
+        Destroy(gameObject, 1f);
     }
 }
